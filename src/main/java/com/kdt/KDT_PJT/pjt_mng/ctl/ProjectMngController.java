c0bd024c2a33afdfd446e0ae92c322688420d9a0
chan <klp100119@gmail.com>
2025-08-05 21:30:28 +0900

    프로젝트관리

--- src/main/java/com/kdt/KDT_PJT/pjt_mng/ctl/ProjectMngController.java
+++ src/main/java/com/kdt/KDT_PJT/pjt_mng/ctl/ProjectMngController.java
@@ -1,16 +1,28 @@
 package com.kdt.KDT_PJT.pjt_mng.ctl;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.kdt.KDT_PJT.cmmn.map.CmmnMap;
+import com.kdt.KDT_PJT.pjt_mng.svc.ProjcetMngService;
 
 @Controller
 @RequestMapping("/pjtMng")
 public class ProjectMngController {
 	
+	@Autowired
+	ProjcetMngService projcetMngService;
 	
 	// log 사용을 위함
 	private final Logger log = LoggerFactory.getLogger(getClass());
@@ -21,9 +33,64 @@ public class ProjectMngController {
 		
 		log.info("getPjtList Called >>> ");
 		
-		// model.addAttribute("name", "홍길동");
-		return "pjt_main";
+		
+		List<CmmnMap> pjtList = projcetMngService.getPjtList();
+		
+		model.addAttribute("pjtList", pjtList);
+		
+		return "pjt_mng/pjt_main";
+	}
+	
+	
+	
+	@GetMapping("/pjtDetail")
+	public String getPjtDetail(@RequestParam("pjtSn") String pjtSn, Model model) {
+	    log.info("getPjtDetail Called >>> " + pjtSn);
+	    
+	    CmmnMap pjtDetail = projcetMngService.getPjtDetail(pjtSn);
+	    model.addAttribute("pjt", pjtDetail);
+
+	    return "pjt_mng/pjt_detail";
 	}
 
 
+	@GetMapping("/getSavePjtForm")
+	public String getSavePjtForm(Model model) {
+		
+		
+		return "pjt_mng/pjt_reg_form";
+
+	}	
+	
+
+	@PostMapping("/savePjtProc")
+	public String savePjtProc(
+			Model model
+			,@RequestParam("pjtNm") String pjtNm,
+		    @RequestParam(value = "empNm", required = false) String empNm,
+		    @RequestParam(value = "pjtBgngDt", required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate pjtBgngDt,
+		    @RequestParam(value = "pjtEndDt", required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate pjtEndDt
+			) {
+		
+		log.info("savePjtProc Called >>> ");
+		
+		CmmnMap params = new CmmnMap();
+		log.info("pjtNm " + pjtNm);
+		log.info("empNm" + empNm);
+		log.info("pjtBgngDt " + pjtBgngDt);
+		log.info("pjtEndDt " + pjtEndDt);
+		
+		
+		params.put("PJT_NM", pjtNm);
+		params.put("EMP_NM", empNm);
+		params.put("PJT_BGNG_DT", pjtBgngDt);
+		params.put("PJT_END_DT", pjtEndDt);
+		
+		
+		projcetMngService.savePjtProc(params);
+		
+		
+		return "redirect:/pjtMng/getPjtList";
+
+	}	
 }
--- src/main/java/com/kdt/KDT_PJT/pjt_mng/svc/ProjcetMngService.java
+++ src/main/java/com/kdt/KDT_PJT/pjt_mng/svc/ProjcetMngService.java
@@ -1,5 +1,35 @@
 package com.kdt.KDT_PJT.pjt_mng.svc;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kdt.KDT_PJT.cmmn.dao.CmmnDao;
+import com.kdt.KDT_PJT.cmmn.map.CmmnMap;
+
+@Service
 public class ProjcetMngService {
+	
+    @Autowired
+    private CmmnDao cmmnDao;
+    
+
+    public List<CmmnMap> getPjtList() {
+        return cmmnDao.selectList("com.kdt.mapper.pjt_mng.getPjtList");
+    }
+
+
+	public CmmnMap getPjtDetail(String pjtSn) {
+		
+		return cmmnDao.selectOne("com.kdt.mapper.pjt_mng.getPjtDetail", pjtSn);
+	}
+
+
+	public void savePjtProc(CmmnMap params) {
+		cmmnDao.insert("com.kdt.mapper.pjt_mng.savePjtProc", params);
+		
+	}
+    
 
 }
