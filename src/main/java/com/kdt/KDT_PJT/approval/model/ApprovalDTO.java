2da18c432c9af15bbead0f661a571dd17b540082
서필규 <user@LAPTOP-GSITM489>
2025-08-06 08:57:18 +0900

    불필요한 예제파일 정리

--- src/main/java/com/kdt/KDT_PJT/approval/model/ApprovalDTO.java
+++ src/main/java/com/kdt/KDT_PJT/approval/model/ApprovalDTO.java
@@ -10,11 +10,4 @@ public class ApprovalDTO {
 	String docId, docType, title, content, department, creatorId, status, writer;
 	Date createdAt;
 	
-	@Override
-	public String toString() {
-		return "ApprovalDTO [docId=" + docId + ", docType=" + docType + ", title=" + title + ", content=" + content
-				+ ", department=" + department + ", creatorId=" + creatorId + ", status=" + status + ", writer="
-				+ writer + ", createdAt=" + createdAt + "]" + "\n";
-	}
-	
 }
