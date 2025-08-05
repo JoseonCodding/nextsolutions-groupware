975edae9a36003af8cff7aebae5bdf95d29e30e8
sohyyyuu <leejj3954@naver.com>
2025-08-05 21:06:36 +0900

    초기화_소현

--- src/main/java/com/kdt/KDT_PJT/cmmn/util/common/MultipartFileList.java
+++
@@ -1,8 +0,0 @@
-package com.kdt.KDT_PJT.cmmn.util.common;
-
-import java.util.ArrayList;
-
-import org.springframework.web.multipart.MultipartFile;
-
-public class MultipartFileList extends ArrayList<MultipartFile> {
-}
