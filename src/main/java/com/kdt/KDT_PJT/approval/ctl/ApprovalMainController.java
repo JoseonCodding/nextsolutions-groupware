65d03573baaa1a747372ebed1cfd65f795feaf50
서필규 <user@LAPTOP-GSITM489>
2025-08-06 15:07:58 +0900

    페이지네이션 구현 완료

--- src/main/java/com/kdt/KDT_PJT/approval/ctl/ApprovalMainController.java
+++ src/main/java/com/kdt/KDT_PJT/approval/ctl/ApprovalMainController.java
@@ -31,19 +31,29 @@ public class ApprovalMainController {
     	int offset = (page - 1) * size;	// 페이지 마다 표시되는 게시글의 시작점 (ex.1페이지:0~9번, 2페이지:10~19번...)
     	int totalCount = approvalMapper.countAll();	// 게시글 DB 전체 개수
     	int totalPages = (int) Math.ceil((double) totalCount / size);	// 전체 페이지 수 ('전체 게시글÷페이지당 게시글 수'를 '올림' 처리) 
+    	int blockSize = 5;	// 페이지네이션에 나타낼 페이지 개수
+    	int startPage, endPage;
+    	
+    	startPage = Math.max(1,  page - 2);	// 페이지네이션에 나타낼 페이지의 첫 페이지 값
+    	endPage = Math.min(totalPages, startPage + blockSize - 1);	// 페이지네이션에 나타낼 페이지의 끝 페이지 값
+		
+    	if ((endPage - startPage + 1) < blockSize && (endPage == totalPages || startPage == 1)) {
+    	    startPage = Math.max(1, endPage - blockSize + 1);
+    	}
+    	
+    	List<ApprovalDTO> approvalData = approvalMapper.pageData(offset, size); // 현재 페이지 게시글 DB 정보 (offset부터 size까지)
+    	
+    	model.addAttribute("approvalData", approvalData);
+    	
+    	model.addAttribute("page", page);
+    	model.addAttribute("totalPages", totalPages);
+    	model.addAttribute("size", size);
+    	model.addAttribute("startPage", startPage);
+    	model.addAttribute("endPage", endPage);
     	
     	System.out.println("	★게시글 DB 전체 개수 : " + totalCount);
     	System.out.println("	★전체 페이지 수 : " + totalPages);
-    	
-    	List<ApprovalDTO> approvalData = approvalMapper.pageData(offset, size); // 현재 페이지 게시글 DB 정보
-
     	System.out.println("	★현재 페이지 게시글 DB 정보 : " + approvalData);
-    	
-    	model.addAttribute("approvalData", approvalData);
-    	model.addAttribute("page", page);
-    	model.addAttribute("totalPages", totalPages);
-    	model.addAttribute("size", size);	
-    	
     	
     	return "approval/approvalMain";
     }
