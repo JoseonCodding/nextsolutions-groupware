e570acab3ba820ad200aeb674942df4f2b256478
서필규 <user@LAPTOP-GSITM489>
2025-08-06 11:29:19 +0900

    전자결제 페이지네이션 작업중1

--- src/main/java/com/kdt/KDT_PJT/approval/ctl/ApprovalMainController.java
+++ src/main/java/com/kdt/KDT_PJT/approval/ctl/ApprovalMainController.java
@@ -3,6 +3,7 @@ package com.kdt.KDT_PJT.approval.ctl;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.kdt.KDT_PJT.approval.mapper.ApprovalMapper;
 
@@ -19,11 +20,18 @@ public class ApprovalMainController {
     }
     
     @RequestMapping("/main")
-    public String approvalMain(Model model) {
+    public String approvalMain(
+			Model model, // 데이터 보관용 (컨트롤러>모델>뷰)
+						 @RequestParam(value = "page", defaultValue = "1") int page, // 페이지네이션용 파라미터 (페이지 번호, 사이즈)
+						 @RequestParam(value = "size", defaultValue = "10") int size) {
+    	
+    	// 데이터베이스 값 모델에 집어넣기
+    	model.addAttribute("approvalData", approvalMapper.selectAllDesc());
 
-    	model.addAttribute("approvalData", approvalMapper.selectAll());
-
-    	System.out.println(approvalMapper.selectAll());
+    	System.out.println("	★게시글 DB 모든 내용" + approvalMapper.selectAllDesc());	// 게시글 DB Console 확인용
+    	
+    	// 페이지네이션	
+    	System.out.println("	★게시글 DB 전체 개수 : " + approvalMapper.countAll());
 
     	return "approval/approvalMain";
     }
--- src/main/java/com/kdt/KDT_PJT/approval/mapper/ApprovalMapper.java
+++ src/main/java/com/kdt/KDT_PJT/approval/mapper/ApprovalMapper.java
@@ -3,6 +3,7 @@ package com.kdt.KDT_PJT.approval.mapper;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 import com.kdt.KDT_PJT.approval.model.ApprovalDTO;
@@ -10,6 +11,16 @@ import com.kdt.KDT_PJT.approval.model.ApprovalDTO;
 @Mapper
 public interface ApprovalMapper {
 	
-	@Select("SELECT * FROM Approval_TEST")
-	List<ApprovalDTO> selectAll();
+	// 데이터베이스 값 불러오기
+	@Select("SELECT * FROM Approval_TEST ORDER BY createdAt DESC")	// 테이블에서 모든 값을 등록일자 기준 내림차순으로 받아옴
+	List<ApprovalDTO> selectAllDesc();
+	
+	
+	// 페이지네이션
+	@Select("SELECT * FROM Approval_TEST ORDER BY createdAt DESC LIMIT #{offset}, #{size}") // offset:시작위치, size:페이지당 글수 (계산은 컨트롤러에서)
+	List<ApprovalDTO> selectPage(@Param("offset") int offset, @Param("size") int size);
+	
+	// 데이터베이스 전체 행 개수
+	@Select("SELECT COUNT(*) FROM Approval_TEST")
+	int countAll();
 }
