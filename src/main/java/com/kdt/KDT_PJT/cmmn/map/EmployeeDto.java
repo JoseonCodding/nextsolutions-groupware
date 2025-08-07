3b1393c1543116077107e45f243c7d93c72def6d
JoseonCodding <playtime997@naver.com>
2025-08-07 16:42:48 +0900

    2025_08_07 5시 결합 commit

--- src/main/java/com/kdt/KDT_PJT/cmmn/map/EmployeeDto.java
+++ src/main/java/com/kdt/KDT_PJT/cmmn/map/EmployeeDto.java
@@ -1,10 +1,13 @@
 package com.kdt.KDT_PJT.cmmn.map;
 
+import java.util.Date;
+
 import lombok.Data;
 
 @Data
 public class EmployeeDto {
 	int empSeq, active;
-	String employeeId, password, empNm;
+	String employeeId, password, empNm, phone, deptName, position, role;
+	Date birth, hireDate, resignDate;
 
 }
