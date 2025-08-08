0522efee60a0369f8c0d020b8760a682bfa4a83a
sohyyyuu <leejj3954@naver.com>
2025-08-08 12:59:05 +0900

    날라간거 일부 수정

--- src/main/java/com/kdt/KDT_PJT/schedule/ctl/ScheduleController.java
+++ src/main/java/com/kdt/KDT_PJT/schedule/ctl/ScheduleController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.kdt.KDT_PJT.schedule.model.ScheduleDTO;
@@ -28,6 +29,15 @@ public class ScheduleController {
 	
 	@GetMapping("/insert")
 	String insert(Model model,  ScheduleDTO dto) {
+		
+		int res = mapper.insert(dto);
+		
+		model.addAttribute("mainUrl", "schedule/insert");
+		return "navTap";
+	}
+	
+	@PostMapping("/insert")
+	String insertReg(Model model,  ScheduleDTO dto) {
 		
 		int res = mapper.insert(dto);
 		
