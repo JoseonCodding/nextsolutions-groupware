e315bbddd24a5cec8e2b6022fbc4c10376b6064b
서필규 <user@LAPTOP-GSITM489>
2025-08-11 15:37:09 +0900

    인터셉터인지 캐리언지 작업중

--- src/main/java/com/kdt/KDT_PJT/config/WebConfig.java
+++ src/main/java/com/kdt/KDT_PJT/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.kdt.KDT_PJT.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -25,6 +26,14 @@ public class WebConfig {
             public void addResourceHandlers(ResourceHandlerRegistry registry) {
                 registry.addResourceHandler("/upload/**")
                         .addResourceLocations("file:///C:/upload/");
+            }
+            
+            
+            @Override
+            public void addInterceptors(InterceptorRegistry registry) {
+                registry.addInterceptor(new AuthInterceptor())
+                        .addPathPatterns("/**")
+                        .excludePathPatterns("/login", "/login/**", "/css/**", "/js/**", "/images/**");
             }
         };
     }
