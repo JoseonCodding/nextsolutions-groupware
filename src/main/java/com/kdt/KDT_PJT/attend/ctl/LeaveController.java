e1d469903a607f3dca63f538641e90e5ed5f6142
sohyyyuu <leejj3954@naver.com>
2025-08-15 16:54:10 +0900

    연차 자동 부여 작업 중

--- src/main/java/com/kdt/KDT_PJT/attend/ctl/LeaveController.java
+++ src/main/java/com/kdt/KDT_PJT/attend/ctl/LeaveController.java
@@ -42,9 +42,9 @@ public class LeaveController {
 	@Resource
 	private Leave leaveService;
 
-	   
-    @Scheduled(cron = "0 0 3 1 * ?") // 매월 1일 새벽 3시에 자동 실행
-   // @Scheduled(cron = "0 */1 * * * ?") // 매 1분마다 실행
+	
+    //@Scheduled(cron = "0 0 3 1 * ?") // 매월 1일 새벽 3시에 자동 실행
+    @Scheduled(cron = "0 */1 * * * ?") // 매 1분마다 실행
     public void scheduleAutoLeave() {
         System.out.println("[스케줄 시작] 전월 근무율 80% 이상 직원 자동 연차 부여 시작");
        leaveService.autoGiveLeaveForQualifiedEmployees();
